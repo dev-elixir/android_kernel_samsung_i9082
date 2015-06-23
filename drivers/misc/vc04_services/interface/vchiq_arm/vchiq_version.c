f71ac689e9ce3ac5b9506a6c25c1319ce932099f
nextgen4k <freshtech100@gmail.com>
2015-06-23 04:06:24 -0400

    some optimizations
    Signed-off-by: nextgen4k <freshtech100@gmail.com>

--- drivers/misc/vc04_services/interface/vchiq_arm/vchiq_version.c
+++ drivers/misc/vc04_services/interface/vchiq_arm/vchiq_version.c
@@ -1,8 +1,8 @@
 #include "vchiq_build_info.h"
 #include <linux/broadcom/vc_debug_sym.h>
 
-VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_hostname, "SEV-DEV-SOLUTION" );
-VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_version, "unknown" );
+VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_hostname, "nextgen4k" );
+VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_version, "a57f3d25ff5e5a2ca61cf3d0e4e7740097617d3b (tainted)" );
 VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_time,    __TIME__ );
 VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_date,    __DATE__ );
 
