629b9824a09ceff3bf9aa2fc29bc8bb554793c69
rishi1998 <freshtech100@gmail.com>
2015-07-15 05:51:09 -0400

    gcc5/6 compilation fixes
    Signed-off-by: rishi1998 <freshtech100@gmail.com>

--- drivers/misc/vc04_services/interface/vchiq_arm/vchiq_version.c
+++ drivers/misc/vc04_services/interface/vchiq_arm/vchiq_version.c
@@ -1,8 +1,8 @@
 #include "vchiq_build_info.h"
 #include <linux/broadcom/vc_debug_sym.h>
 
-VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_hostname, "SEV-DEV-SOLUTION" );
-VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_version, "unknown" );
+VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_hostname, "nextgen4k" );
+VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_version, "f6a6c9e638e307dd9777e24072d26d023400649b (tainted)" );
 VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_time,    __TIME__ );
 VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_date,    __DATE__ );
 
